f1af25b141638d4b1c87845220691d628c2b56ff
unknown <junmo1225@daum.net>
2020-10-25 21:07:15 +0900

    popup

---
+++ benefit_page.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>11월 창업 혜택</title>
+</head>
+
+<body>
+
+
+
+<style>
+
+/*초기화코드*/
+/* http://meyerweb.com/eric/tools/css/reset/
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+*{
+	padding:0;
+	margin:0;
+	list-style:none;
+	text-decoration:none;
+} /* 전체 초기화 */
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {display: block;}
+ol, ul {list-style: none;}
+blockquote, q {quotes: none;}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+/* map태그 테두리 none */
+area:focus{
+  border: none;
+  outline:none;
+  outline-style: none;
+  -moz-outline-style:none;
+}
+img,
+img a{
+	outline: none !important;
+	border: none !important;
+}
+
+
+body{
+	width: 100%;
+	z-index: -3;
+	background-color: transparent;
+}
+
+.content{
+	width: 100%;
+	height: auto;
+	overflow: hidden;
+	margin: 0 auto;
+	text-align: center;
+	font-size: 0;
+	position: relative;
+}
+.content img{
+	width: 100%;
+	height: auto;
+}
+.content a{
+	position: absolute;
+	bottom: 1%;
+	right: 0;
+	left: 0;
+	margin: 0 auto;
+	width: 46%;
+	height: 5.5%;
+}
+
+</style>
+	<div class="content">
+		<img src="newimg/benefit/img.jpg" alt="#img" width="1920" height="4069">
+		<a class="cont_btn_box" href="http://www.hunterpc.co.kr/hunter2/bbs/write.php?bo_table=qa&device=pc"></a>
+	</div>
+</body>
+</html>
